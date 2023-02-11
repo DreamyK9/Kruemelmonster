6adcb77098c1414c1a677949473f8abecbe6490b
DreamDev621 <it.wolfx64@gmail.com>
2023-02-11 19:19:10 +0100

    restructured project (overview in OVERVIEW.md)

---
+++ OVERVIEW.md
@@ -0,0 +1,2 @@
+* [index.js](index.js) - Hier werden Aktionen auf der aktuellen Seite ausgeführt, wie bspw. das klicken auf einen Button
+* [popup-Ordner](popup/index.html) - Hier befindet sich der Code für das Popup, das beim Klicken auf das Icon in der Toolbar erscheint (ist genauso wie eine Webseite, mit HTML, CSS und JS)
--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-if (window.location.host === "www.google.de") {
-  var declineButton = document.querySelector("#W0wltc");
-  if (declineButton) {
+// Code, der auf der Seite ausgeführt wird
+// Hier kommt der Code zum drücken der Cookie-Banner hin
+
+let buttonClicked = false;
+let declineButton;
+
+// URL überprüfen und passenden Button auswählen
+switch (window.location.host) {
+    case "www.google.de":
+        declineButton = document.querySelector("#W0wltc");
+        break;
+    case "www.google.com":
+        declineButton = document.querySelector("#W0wltc");
+        break;
+    case "www.ecosia.org":
+        declineButton = document.querySelector(".cookie-consent__action");
+        break;
+}
+
+
+if (!buttonClicked) {
     // Automatisches Klicken des "Ablehnen"-Buttons
     declineButton.click();
-  }
+    buttonClicked = true;
 }
