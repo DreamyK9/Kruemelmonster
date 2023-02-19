1b2143c0b1f8f89c9ee7afeafff95e1f42739140
DreamDev621 <it.wolfx64@gmail.com>
2023-02-19 17:55:50 +0100

    added system for multiple actions per banner

--- index.js
+++ index.js
@@ -2,52 +2,127 @@
 // Here goes the code for dealing with cookie banners
 
 
-let rules = [
+// possible sector types
+const Selector = {
+    class: "class",
+    id: "id",
+    xml: "xml",
+}
+
+// possible action types
+const Action = {
+    click: "click",
+}
+
+const rules = [
     {
-        "url": /www\.google\.[a-z]{2,}/,
-        "selector": "#W0wltc"
+        "host": /www\.google\.[a-z]{2,}/,
+        "actions": [
+            {
+                "type": Action.click,
+                "delay": 0,
+                "selector": {
+                    "query": "W0wltc",
+                    "type": Selector.id
+                }
+            }
+        ]
     },
     {
-        "url": "www\.ecosia\.org",
-        "selector": ".cookie-consent__action"
+        "host": "www.ecosia.org",
+        "actions": [
+            {
+                "type": Action.click,
+                "delay": 0,
+                "selector": {
+                    "query": "cookie-consent__action",
+                    "type": Selector.class
+                }
+            }
+        ]
     },
     {
-        "url": "www.bing.com",
-        "selector": "#bnp_btn_reject"
+        "host": "www.bing.com",
+        "actions": [
+            {
+                "type": Action.click,
+                "delay": 1600,
+                "selector": {
+                    "query": "bnp_btn_reject",
+                    "type": Selector.id
+                }
+            }
+        ]
     },
     {
-        "url": "www.microsoft.com",
-        "selector": "._1XuCi2WhiqeWRUVp3pnFG3"
+        "host": "www.microsoft.com",
+        "actions": [
+            {
+                "type": Action.click,
+                "delay": 600,
+                "selector": {
+                    "query": "_1XuCi2WhiqeWRUVp3pnFG3",
+                    "type": Selector.class
+                },
+            }
+        ]
     }
 ]
 
-
-let declineButton = undefined;
 let foundRule = undefined;
 
 foundRule = findMatchingRule();
 
 if (foundRule) {
-    declineBanner();
+    declineBanner(foundRule);
 }
 
 /* -------------------------------------------------------------------------- */
 
 function findMatchingRule() {
     for (let r of rules) {
-        if (window.location.host.match(r.url))
+        if (window.location.host.match(r.host)) {
             return r;
+        }
     }
 }
 
 
-function declineBanner() {
-    let declineButton = document.querySelector(foundRule.selector);
-
-    if (declineButton) {
-        declineButton.click();
-    } else {
-        // try again in 1 second
-        setTimeout(declineBanner, 1000);
+function declineBanner(rule) {
+    for (action of rule.actions) {
+        switch (action.type) {
+            case Action.click:
+                setTimeout(() => {
+                    clickButton(action.selector);
+                }, action.delay);
+                break;
+        }
     }
+}
+
+function clickButton(selector) {
+    let button = getButton(selector);
+
+    if (button) {
+        button.click();
+    }
+}
+
+
+function getButton(selector) {
+    let button = undefined;
+
+    switch (selector.type) {
+        case Selector.class:
+            button = document.getElementsByClassName(selector.query)[0];
+            break;
+        case Selector.id:
+            button = document.getElementById(selector.query);
+            break;
+        case Selector.xml:
+            throw new Error("XML selectors are not supported yet");
+            break;
+    }
+
+    return button;
 }
