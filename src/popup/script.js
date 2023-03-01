8fc1628ef651d1ccb45184db51f5755657275f8a
FILUSU <85636655+FILUSU@users.noreply.github.com>
2023-03-01 18:03:28 +0100

    Update script.js

--- src/popup/script.js
+++ src/popup/script.js
@@ -1,13 +1,12 @@
 // Code for the popup window goes here
 async function sendreport(params) {
-    document.getElementById("report").addEventListenerListener("click", function () {
-        var xhr = new XMLHttpRequest();
-        xhr.open("Get", "http://localhost:3000", true);
-        xhr.send();
-    });
     let response = await fetch("https://api.chucknorris.io/jokes/random");
     let data = await response.json();
     document.getElementById('toll').innerHTML = data.value;
+    //Ab hier wird es wichtig
+    var xhr = new XMLHttpRequest();
+    xhr.open("Get", "http://localhost:3000", true);
+    xhr.send();
 }
 
 
